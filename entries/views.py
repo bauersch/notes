86c2b8a46d3a863c70080b08e5723b8dd2fac93e
bauersch <97994285+bauersch@users.noreply.github.com>
2022-01-25 18:11:38 -0600

    created list and deatel view nad created templates

--- entries/views.py
+++ entries/views.py
@@ -5,3 +5,7 @@ from django.views.generic import(ListView,DetailView)
 from .models import Entry
 class EntryListView(ListView):
     model=Entry
+    queryset=Entry.objects.all().order_by("-date_created")
+
+class EntryDetailView(DetailView):
+    model=Entry
